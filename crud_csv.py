d6bec61b928bc3577461f9b3471e041c9382d729
kevinBonifacio25 <127898634+kevinBonifacio25@users.noreply.github.com>
2026-04-06 12:41:14 -0500

    Update crud_csv.py

--- crud_csv.py
+++ crud_csv.py
@@ -3,7 +3,7 @@ import os
 
 
 
-ARCHIVO_DATA1 = "clientes_csv.csv"
+ARCHIVO_DATA1 = "producto_csv.csv"
 
 def cargar_datos_csv ():
     if not os.path.exists(ARCHIVO_DATA1):
@@ -14,23 +14,23 @@ def cargar_datos_csv ():
             #
             lector = csv.DictReader(f)
             for fila in lector:
-                lista_clientes.append(fila)
+                lista_producto.append(fila)
         print("Datos cargardos")
     except FileNotFoundError:
         print("el archivo no existe")
-    return lista_clientes        
+    return lista_producto        
 
 
-def guardar_clientes_csv(lista_clientes):
+def guardar_producto_csv(lista_producto):
     ARCHIVO_DATA1
 
-    nombres_columnas = lista_clientes[0].keys()
+    nombres_columnas = lista_producto[0].keys()
 
     with open(ARCHIVO_DATA1, mode='w', newline='',encoding='utf-8')as f:
         escritor = csv.DictWriter(f,fieldnames= nombres_columnas)
 
         escritor.writeheader()
-        escritor.writerows(lista_clientes)
+        escritor.writerows(lista_producto)
 
     print(f"datos guardados{ARCHIVO_DATA1}")
     
