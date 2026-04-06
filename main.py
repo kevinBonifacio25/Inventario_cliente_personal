882234b44d3a9bd7b4bf8edcf69b5e4dc258d916
kevinBonifacio25 <127898634+kevinBonifacio25@users.noreply.github.com>
2026-04-06 12:39:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import crud_csv
 import prueba
 
 def menu ():
-    lista_clientes = crud.cargar_datos() 
+    lista_producto = crud.cargar_datos() 
     opciones ="" 
     while opciones != "8":
 
-        print("Marca 1 para registrar usuario")
-        print("Marca 2 para mostrar usuarios")
-        print("Marca 3 para buscar ususario")
-        print("Marca 4 para actualizar usuario")
-        print("Marca 5 para eliminar usuario")
+        print("Marca 1 para registrar producto")
+        print("Marca 2 para mostrar producto")
+        print("Marca 3 para buscar producto")
+        print("Marca 4 para actualizar producto")
+        print("Marca 5 para eliminar producto")
         print("Marca 6 para guardar archivo en formato json")
         print("Marca 7 para cargar archivo en formato json")
         print("Marca 8 para cargar archivo en formato csv")
@@ -22,34 +22,34 @@ def menu ():
 
         match opciones:
             case "1" :
-                prueba.registrar_clientes(lista_clientes)
+                prueba.registrar_clientes(lista_producto)
 
             case "2":
-                prueba.listar_clientes(lista_clientes)
+                prueba.listar_clientes(lista_producto)
 
             case "3":
-                prueba.buscar_clientes_por_id(lista_clientes)
+                prueba.buscar_clientes_por_id(lista_producto)
 
             case "4":
-                prueba.actualizar_info_clientes(lista_clientes)    
+                prueba.actualizar_info_clientes(lista_producto)    
 
             case "5":    
-                prueba.eliminar_cliente(lista_clientes)
+                prueba.eliminar_cliente(lista_producto)
 
             case "6":
-                crud.guardar_datos(lista_clientes)
+                crud.guardar_datos(lista_producto)
                 print("Archivo guardado exitosamente en formato JSON...")
 
             case "7":
-                lista_clientes = crud.cargar_datos()      
+                lista_producto = crud.cargar_datos()      
                 print("Archivo cargado exitosamente en formato JSON...")
 
             case "8":
-                lista_clientes = crud_csv.cargar_datos_csv()
+                lista_producto = crud_csv.cargar_datos_csv()
                 print("Archivo cargado exitosamente en formato CSV...")    
 
             case "9":
-                lista_clientes = crud_csv.guardar_clientes_csv(lista_clientes)
+                lista_producto = crud_csv.guardar_clientes_csv(lista_producto)
                 print("Archivo guardado exitosamente en formato CSV...")
 
             case "0":
